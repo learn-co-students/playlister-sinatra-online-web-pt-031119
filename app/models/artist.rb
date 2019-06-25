5588d4232417b8f155d5798292d2f53a898e2c82
Alex Merecka <merecka@gmail.com>
2019-06-25 15:44:33 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,21 @@
 class Artist < ActiveRecord::Base
 	has_many :songs
 	has_many :genres, through: :songs
+
+	def slug
+		slug_array = self.name.split(/\s*\W/).keep_if {|v| v=~/\w/} #forms array of individual words that only include word characters (letters, numbers, underscores)
+		slug_name = slug_array.join("-").downcase #creates a string with '-' in between words
+	end	
+
+	def self.find_by_slug(slug)
+		found_artist = nil
+		Artist.all.collect do |artist|
+			# binding.pry
+			if artist.slug == slug
+				found_artist = artist
+			end
+		end
+		# binding.pry
+		found_artist
+	end
 end
