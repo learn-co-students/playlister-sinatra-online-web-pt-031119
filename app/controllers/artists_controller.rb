8102eab4209d5bce0c7469891c119764d594e38e
“Mark <mkanko66@gmail.com>
2020-04-27 10:52:25 -0700

    sinatra review, lab completed

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,45 @@
 class ArtistsController < ApplicationController
+
+    # get '/' do 
+    #     redirect to "/artists"
+    # end 
+
+    get '/artists' do 
+        @artists = Artist.all 
+        erb :'/artists/index' 
+    end 
+
+    get '/artists/new' do 
+        # @artist = Artist.new 
+        erb :'/artists/new' 
+    end 
+
+    post '/artists' do 
+        @artist = Artist.create(params[:artist])
+        redirect to "/artists/#{@artist.slug}"
+    end 
+
+    get '/artists/:slug' do 
+        @artist = Artist.find_by_slug(params[:slug])
+        erb :'/artists/show' 
+    end 
+
+    get '/artists/:slug/edit' do
+        @artist = Artist.find(params[:id]) 
+        erb :'/artists/edit' 
+    end 
+
+    patch 'artists/:slug' do 
+        @artist = Artist.find(params[:id])
+
+        redirect to "artists/#{@artist.slug}"
+    end 
+
+    delete '/artists/:slug' do
+        Article.destroy(param[:id])
+        redirect to "/artists"
+    end 
+
+     
+
 end
---
+++ app/models/genre.rb
@@ -0,0 +1,9 @@
+class Genre < ActiveRecord::Base 
+    has_many :song_genres
+    has_many :songs, through: :song_genres 
+    has_many :artists, through: :songs
+    
+    extend Sluggable::ClassMethods
+    include Sluggable::InstanceMethods
+    
+end 
